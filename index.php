7f031750743e171d0791bd35d3158b08a82d84a6
MilenaSchmidt <milenaschmidt@protonmail.com>
2021-03-12 11:33:00 +0100

    DI BaseRequest in Router class

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 require __DIR__ . '/config/bootstrap.php';
+use PhpModularity\Requests\BaseRequest;
 
 $router = require __DIR__ . '/src/Routing/web.php';
-$router->direct($_SERVER['REQUEST_URI']);
+
+$router->direct(new BaseRequest($_REQUEST, $_SERVER['REQUEST_URI']));
 
