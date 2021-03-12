240f46bc13c5c9c8250afdbcc555a4e63abedbdf
MilenaSchmidt <milenaschmidt@protonmail.com>
2021-03-12 11:27:44 +0100

    request class and interface

---
+++ src/Requests/Interfaces/RequestInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpModularity\Requests\Interfaces;
+
+interface RequestInterface
+{
+    public function getUrl(): string;
+
+    public function getParameter(string $parameter): array;
+
+    public function only(array $parameters): array;
+
+    public function except(array $parameters): array;
+
+}
