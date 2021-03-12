3d405ce24b5ddb5c89ab1dc92bc1d64f9f318dda
MilenaSchmidt <milenaschmidt@protonmail.com>
2021-03-12 13:48:26 +0100

    dotenv installation

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 require __DIR__ . '/config/bootstrap.php';
 use PhpModularity\Requests\BaseRequest;
+$dotenv = Dotenv\Dotenv::createImmutable(realpath($_SERVER["DOCUMENT_ROOT"]));
+$dotenv->load();
 
-$router = require __DIR__ . '/src/Routing/web.php';
+
+$router = require(__DIR__ . '/src/Routing/web.php');
 
 $router->direct(new BaseRequest($_REQUEST, $_SERVER['REQUEST_URI']));
 
